a3ac8e783fba8cd8421c06c33fb8d35461b44538
Gukanr007 <gukanranganathan@gmail.com>
2024-07-24 23:48:46 +0530

    first commit

---
+++ lib/screens/conversation.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Conversation extends StatefulWidget {
+  const Conversation({super.key});
+
+  @override
+  State<Conversation> createState() => _ConversationState();
+}
+
+class _ConversationState extends State<Conversation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 15),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(image: NetworkImage(""))),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
